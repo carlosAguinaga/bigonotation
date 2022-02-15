6f545da7c7d44eb935954e230d260b88d638e561
Carlos Aguinaga <50058407+carlosAguinaga@users.noreply.github.com>
2022-02-15 18:25:43 -0500

    Update example2.js

--- example2.js
+++ example2.js
@@ -1,9 +1,10 @@
 function printPairs(array){
-  for(let i = 0; i < array.length; i++){
-    for(let j = 0; j < array.length; j++){
-      console.log(`${array[i]}, ${array[j]}`)
+  for(let i = 0; i < array.length; i++){   // O(n)
+    for(let j = 0; j < array.length; j++){  // O(n)
+      console.log(`${array[i]}, ${array[j]}`)  // O(n)
     }
   }
 }
 
 //¿Cuál es la complejidad de tiempo?
+//la complejidad es de // O(n + n ^ 2) -> // O(n ^ 2)
