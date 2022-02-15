f6398436866ec562801e1842ec0f3a8a7fcba508
Carlos Aguinaga <50058407+carlosAguinaga@users.noreply.github.com>
2022-02-15 18:38:01 -0500

    Update example5.js

--- example5.js
+++ example5.js
@@ -1,11 +1,13 @@
 function printUnorderedPairs(arrayA, arrayB){
-  for(let i = 0; i < arrayA.length; i++){
-    for(let j = 0; j < arrayB.length; j++){
-      for(let k = 0; k < 100000; k++){
-        console.log(`${arrayA[i]}, ${arrayB[j]}`);
+  for(let i = 0; i < arrayA.length; i++){  // O(n)
+    for(let j = 0; j < arrayB.length; j++){ // O(k)
+      for(let k = 0; k < 100000; k++){  // O(100000)
+        console.log(`${arrayA[i]}, ${arrayB[j]}`); // O(100000)
       }
     }
   }
 }
 
 //¿Cuál es la complejidad de tiempo?
+
+//la complejidad es de // O(200000 + nk) -> // O(n ^ 2)
