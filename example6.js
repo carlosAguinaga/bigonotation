d4f540ebf55c3f73bd6fb23df1d446633a6e1393
Carlos Aguinaga <50058407+carlosAguinaga@users.noreply.github.com>
2022-02-15 18:43:17 -0500

    Update example6.js

--- example6.js
+++ example6.js
@@ -1,6 +1,6 @@
 function reverse(array){
-  for(let i = 0; i < array.length / 2; i++){
-    let other = array.length - i - 1;
+  for(let i = 0; i < array.length / 2; i++){  // O(n / 2)
+    let other = array.length - i - 1; // O(n - n/2 - 1)
     let temp = array[i];
     array[i] = array[other];
     array[other] = temp;
@@ -8,3 +8,5 @@ function reverse(array){
 }
 
 //¿Cuál es la complejidad de tiempo?
+
+//la complejidad es de // O(n)
